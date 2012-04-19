08445ce217934aa871cef3154cc083ae1fdc003f
Daniel Kehoe <daniel@danielkehoe.com>
2012-04-19 14:08:57 -0700

    use 'alert' not 'error' for flash messsge on CanCan::AccessDenied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_path, :error => exception.message
+    redirect_to root_path, :alert => exception.message
   end
 end
