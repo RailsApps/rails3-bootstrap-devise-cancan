10cedc5b9137805b8607f4b5bfdd9a1144100087
Daniel Kehoe <daniel@danielkehoe.com>
2012-12-17 17:41:11 -0800

    use 'find_or_create_by_email' instead of 'find_or_create_by_name'

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,6 @@ YAML.load(ENV['ROLES']).each do |role|
   puts 'role: ' << role
 end
 puts 'DEFAULT USERS'
-user = User.find_or_create_by_name :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
 user.add_role :admin
