80a54f0c60f76facf22bcc293f01c91549115131
joohee <neigie@gmail.com>
2013-12-06 19:20:33 +0900

    add rails4 configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,13 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, :alert => exception.message
   end
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.for(:sign_in) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
+  end
+
 end
