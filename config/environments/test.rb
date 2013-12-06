6df94607b936c6ae74dc61ba843fded5ca088d12
joohee <neigie@gmail.com>
2013-12-06 14:26:50 +0900

    remove unused options of rails 4

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,9 +32,6 @@ Rails3BootstrapDeviseCancan::Application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # Raise exception on mass assignment protection for Active Record models
-  #config.active_record.mass_assignment_sanitizer = :strict
-
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 
