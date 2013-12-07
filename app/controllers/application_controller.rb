df23bc73cff5c21444c310596dcbc06c76ff20e3
joohee <neigie@gmail.com>
2013-12-07 10:41:23 +0900

    not sign in, but sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-  	devise_parameter_sanitizer.for(:sign_in) << :name
+  	devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:account_update) << :name
   end
 
