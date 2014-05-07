410b98bd2cb0037ff7e8f43a545125bb8f9de1f5
sbezboro <sergei@ratchet.io>
2014-05-07 13:04:13 -0700

    new deploy hook method

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,18 +1,4 @@
-require 'net/http'
-require 'uri'
-
-uri = URI.parse('https://api.rollbar.com/api/1/deploy/')
-
-params = {
-    :local_username => config.deployed_by,
-    :access_token => ENV['ROLLBAR_ACCESS_TOKEN'],
-    :environment => config.framework_env,
-    :revision => config.revision
-}
-
-request = Net::HTTP::Post.new(uri.request_uri)
-request.body = JSON.dump(params)
-
-Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
-    http.request(request)
+on_app_master do
+  run "curl https://api.rollbar.com/api/1/deploy/ --silent -F access_token=#{ENV['ROLLBAR_ACESS_TOKEN']} -F environment=#{config.environment} -F revision=#{config.revision}"
 end
+
