09151e2f1ce48648fac5dc690d1356711afc4d5a
sbezboro <sergei@ratchet.io>
2014-05-07 13:08:59 -0700

    fix typo

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,4 +1,4 @@
 on_app_master do
-  run "curl https://api.rollbar.com/api/1/deploy/ --silent -F access_token=#{ENV['ROLLBAR_ACESS_TOKEN']} -F environment=#{config.environment} -F revision=#{config.revision}"
+  run "curl https://api.rollbar.com/api/1/deploy/ --silent -F access_token=#{ENV['ROLLBAR_ACCESS_TOKEN']} -F environment=#{config.environment} -F revision=#{config.revision}"
 end
 
